ddb6c6e89b7f0504c190ea6992f4728c7f081675
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2020-07-26 17:37:55 -0300

    Tela de visualização do Post

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // import 'dart:html';
 import 'package:flutter/material.dart';
-import 'package:inov_connect/screens/users/forgot.dart';
+import 'package:inov_connect/screens/posts/descpost.dart';
 
 void main() => runApp(InovConnect());
 
@@ -12,7 +12,7 @@ class InovConnect extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.grey[50],
       ),
-      home: Forgot(),
+      home: DescPost(),
     );
   }
 }
