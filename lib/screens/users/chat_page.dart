98f0ac6c5ff18c1873ffbb525462cd0e98c8a3f4
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2020-08-09 22:13:49 -0300

    Tela de Chat

--- lib/screens/users/chat_page.dart
+++ lib/screens/users/chat_page.dart
@@ -9,13 +9,194 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlue[300],
-        title: Text('Chat UOL'),
+      backgroundColor: Colors.lightBlue[400],
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 8.0, 8.0, 8.0),
+              child: Row(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(36.0),
+                    ),
+                    child: Image.asset(
+                      'assets/images/person64.jpg',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30.0, 0, 0, 0),
+                    child: Text(
+                      "Guilherme L.Fernandez",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0, 8.0),
+                    child: Container(
+                      //margin:,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(5),
+                          topRight: const Radius.circular(5),
+                          bottomLeft: const Radius.circular(5),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Flexible(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(8.0, 8.0, 0, 8.0),
+                              child: Text(
+                                'Pessoa que enviou direita',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ),
+                          // Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 40, 5, 5),
+                            child: Text(
+                              '11:20',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 8.0, 8.0, 8.0),
+                    child: Container(
+                      //margin:,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(5),
+                          topRight: const Radius.circular(5),
+                          bottomRight: const Radius.circular(5),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Flexible(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(8.0, 8.0, 0, 8.0),
+                              child: Text(
+                                'Pessoa que enviou esquerda',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ),
+                          // Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 40, 5, 5),
+                            child: Text(
+                              '11:20',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(8.0),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Digite alguma coisa..',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            8.0,
+                          ),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.lightBlue[400],
+                        ),
+                        borderRadius: new BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+                  child: ButtonTheme(
+                    height: 30,
+                    minWidth: 90,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
+                      onPressed: () {},
+                      color: Color.fromARGB(255, 58, 137, 193),
+                      child: Text(
+                        'Enviar',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(child: Text("Esta é a tela de Chat "),),
     );
   }
 }
-
-
